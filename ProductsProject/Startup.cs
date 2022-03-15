4515d10ffc3a750f9b98893da270fd459336c3a0
nikita <sabaoth@ukr.net>
2022-03-15 22:55:13 +0200

    repository completed

--- ProductsProject/Startup.cs
+++ ProductsProject/Startup.cs
@@ -13,6 +13,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Core.Interfaces;
 
 namespace ProductsProject
 {
@@ -29,7 +30,7 @@ namespace ProductsProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddScoped<IProductRepository, ProductRepository>(); 
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
